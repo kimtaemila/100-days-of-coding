e0128a2fea69eb360efa0f92401d4af4076f2186
kimtaemila <rudmilaarafin@gmail.com>
2023-01-15 14:38:45 -0500

    Day 2 updated

--- Day2/main.py
+++ Day2/main.py
@@ -1,5 +1,4 @@
 #If the bill was $150.00, split between 5 people, with 12% tip. 
-
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Format the result to 2 decimal places = 33.60
 
@@ -7,13 +6,11 @@
 
 #Write your code below this line 👇
 print("Welcome to the tip calculator.")
-bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-# tip_percent = tip_as_float/100
-# tip_total = bill_as_float*tip_percent
-# total_bill = bill_as_float + tip_total
+total_bill = float(input("What was the total bill? "))
+tips = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill? "))
-bill_with_tip = ((tip/100) *bill) + bill
-bill_per_person = bill_with_tip / people
-final_amount = round(bill_per_person, 2)
-print(f"Each person should pay: ${final_amount}")
+bill_with_tip = (total_bill * (tips / 100)) + total_bill
+split_with_people = bill_with_tip / people
+total_amount = round(split_with_people, 2)
+total_amount = "{:.2f}".format(split_with_people)
+print(f"Each person should pay ${total_amount}")
